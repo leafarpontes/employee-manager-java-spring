faa2be678f393215f9e5816f0246e793bac8c88b
leafarpontes <rafapontes2008@hotmail.com>
2022-05-16 23:01:41 -0300

    Pushing initial structure to remote. (generated by spring initializr)

---
+++ src/test/java/pontes/rafael/employeemanager/EmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pontes.rafael.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
